830cbf6194188f8a3d4f272b5e3be87826e64ae0
SkmtJun <fy3277612@hotmail.com>
2021-05-13 00:11:19 +0800

    更新 2021/05/13 周四  0:11:19.40

--- QuickUpload.bat
+++ QuickUpload.bat
@@ -2,9 +2,21 @@ call hexo clean
 call hexo g
 call hexo d
 @call hexo clean
-echo Blog已部署
+
+@echo ********************
+@echo.
+@echo       Blog已部署
+@echo.
+@echo ********************
+
 call git add .
 call git commit -m  "更新 %date% %time%"
 call git push
-echo 已push至Github
+
+@echo ********************
+@echo.
+@echo   已push至Github
+@echo.
+@echo ********************
+
 @pause
